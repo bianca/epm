a44c3253e0cdae8581b9764933d15ee8e568b2a9
bianca <bianca.sayan@gmail.com>
2015-09-09 12:47:23 -0400

    add api key

--- config/initializers/geokit_config.rb
+++ config/initializers/geokit_config.rb
@@ -1,4 +1,4 @@
-Geokit::Geocoders::NominatimGeocoder.server = 'open.mapquestapi.com/nominatim/v1/search'
+Geokit::Geocoders::NominatimGeocoder.server = 'open.mapquestapi.com/nominatim/v1/search?key=cYOondyn4ktJAf7frbiVD5jkdGNASuf0'
 
 Epm::Application.configure do
   config.geokit.default_units = :kms
