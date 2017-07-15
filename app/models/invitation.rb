f2c0511a1e80313dc9f504df8544c83ba47e7017
bianca <bianca.sayan@gmail.com>
2017-07-15 12:38:40 -0400

    Prevents 2 or more invitations with the same user_id and event_id

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -4,6 +4,7 @@ class Invitation < ActiveRecord::Base
   belongs_to :event
 
   validates :user_id, :event_id, presence: true
+  validates :user_id, uniqueness: { scope: :event }
 
   before_save do |invitation|
     invitation.send_by = Time.zone.now unless invitation.send_by.present?
